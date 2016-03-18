14a9a2ee2cb0fec2303fff3e961cd8000332071b
JeerawatIT_Innovation <gukaihero@gmail.com>
2016-03-18 16:26:30 +0700

    Create Tabs Navigation

--- app/src/main/java/com/jeerawatdev/useribm/smartqandorder/MainActivity.java
+++ app/src/main/java/com/jeerawatdev/useribm/smartqandorder/MainActivity.java
@@ -1,8 +1,6 @@
 package com.jeerawatdev.useribm.smartqandorder;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private TabLayout tabLayout;
+    private int[] tabIcons = {
+            R.drawable.ic_exterior,
+            R.drawable.ic_qr_code,
+            R.drawable.ic_myspace,
+            R.drawable.ic_perchase_order
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,18 +51,26 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+        SetupTabIcons();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
+    }
+
+    private void SetupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
     }
 
 
@@ -77,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
@@ -121,18 +135,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "IC_1";
-                case 1:
-                    return "IC_2";
-                case 2:
-                    return "IC_3";
-                case 3:
-                    return "IC_4";
-
-            }
+//            switch (position) {
+//                case 0:
+//                    return "IC_1";
+//                case 1:
+//                    return "IC_2";
+//                case 2:
+//                    return "IC_3";
+//                case 3:
+//                    return "IC_4";
+//
+//            }
             return null;
         }
+
+
     }
 }
