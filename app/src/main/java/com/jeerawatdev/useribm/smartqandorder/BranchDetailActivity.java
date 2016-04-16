de3fe6d36613c797cace88ecd3547b2f02938460
JeerawatDev <gukaihero@gmail.com>
2016-04-16 22:51:46 +0700

    CreatePage SubHome

---
+++ app/src/main/java/com/jeerawatdev/useribm/smartqandorder/BranchDetailActivity.java
@@ -0,0 +1,13 @@
+package com.jeerawatdev.useribm.smartqandorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BranchDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_branch_detail);
+    }
+}
---
+++ app/src/main/java/com/jeerawatdev/useribm/smartqandorder/ReserveQueueActivity.java
@@ -0,0 +1,13 @@
+package com.jeerawatdev.useribm.smartqandorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReserveQueueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_queue);
+    }
+}
